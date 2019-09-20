f45204cc718ac5123c481070192cd91be5142fb8
cristian rodriguez <cristianrodriguez@MacBook-Air-de-cristian.local>
2019-09-20 01:51:04 -0300

    funcionando css, no funciona shell

--- desktop_app/index.js
+++ desktop_app/index.js
@@ -1,5 +1,8 @@
 let request = require('request');
+const { shell } = require('electron')
 window.$ = window.jQuery = require('jquery');
+
+shell.openExternal('http://google.cl');
 
 $(document).ready(function () {
     $("#myInput").on("keyup", function () {
@@ -17,6 +20,11 @@ $(document).ready(function () {
     });
 });
 
+function OpenExternal(url)
+{
+    shell.openExternal(url);
+}
+
 function makeRequest()
 {
     request("http://127.0.0.1:2222/api/jobs/tasks", function(err, response, body){
@@ -31,11 +39,13 @@ function makeRequest()
             classStatus = "tareaTerminada";
         }
 
-
-        randomQuote += "<div id ='" + task["ID"] + "' class='" + classStatus + "'" +task["ID"] + ">";
-        randomQuote += " " + task["estado"];
-        randomQuote += " " + task["name"];
-        randomQuote += "</div><br>";
+        randomQuote += "<div id ='" + task["ID"] + "' class='cuadroTarea " + classStatus + "'>";
+        randomQuote += "<div class='topLeft'>" + task["name"] + "</div>";
+        randomQuote += "<div class='topRight'>"  + task["estado"] + "</div>";
+        randomQuote += "<div class='bottomLeft'>ID "  + task["ID"] + "</div>";
+        randomQuote += "<div class='bottomRight'>Creación: "  + task["creation"] + "</div>";
+        randomQuote += "<div class='center'>blabla <br><a href=javascript:OpenExternal('http://127.0.0.1:1111/taskStatus/" + task["ID"] + "') target='_blank' class='button'>Ver detalle</a><button> ver detalle </button></div>";
+        randomQuote += "</div>";
     });
 
     document.getElementById("quote").innerHTML = randomQuote;
@@ -43,7 +53,7 @@ function makeRequest()
 }
 
 makeRequest();
-
+/*
 setInterval(function(){
     makeRequest();
-}, 5000);
+}, 5000);*/
